03985f7c59c989adcf0fa9d97bd4a3978b860abb
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-01 15:02:37 -0300

    Typography

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import Typography from "@material-ui/core/Typography";
 
 export default function Create() {
   return (
     <div>
-      Create page
+      <Typography
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a New note
+      </Typography>
     </div>
-  )
+  );
 }
