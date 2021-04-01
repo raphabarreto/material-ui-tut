e340b2cb66f8e200cc4448fca18602977b3d8ef2
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-01 18:56:46 -0300

    makeStyles Hook (Custom CSS)

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,13 +1,31 @@
+import React from "react";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
-import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  button: {
+    fontSize: 60,
+    backgroundColor: "violet",
+    "&:hover": {
+      backgroundColor: "blue",
+    },
+  },
+  title: {
+    textDecoration: "underline",
+    marginBottom: 20,
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+
   return (
     <Container>
       <Typography
+        className={classes.title}
         variant="h6"
         component="h2"
         gutterBottom
@@ -17,6 +35,7 @@ export default function Create() {
       </Typography>
 
       <Button
+        className={classes.button}
         onClick={() => console.log("you clicked me")}
         type="submit"
         color="secondary"
