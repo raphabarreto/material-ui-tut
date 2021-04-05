e96d117955a69f219d62c199e5e8b6d4597bba30
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-04 23:39:36 -0300

    Using JSON Server

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -9,6 +9,7 @@ import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControl from "@material-ui/core/FormControl";
 import FormLabel from "@material-ui/core/FormLabel";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   field: {
@@ -20,6 +21,7 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles();
+  const history = useHistory();
 
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
@@ -42,7 +44,11 @@ export default function Create() {
     }
 
     if (title && details) {
-      console.log(title, details, category);
+      fetch("http://localhost:8000/notes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title, details, category }),
+      }).then(() => history.push("/"));
     }
   };
 
