6f4fa30128d17c7fa0afb9ab3d84a42743989f58
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-05 16:42:59 -0300

    Permanent Drawer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
+import Drawer from "@material-ui/core/Drawer";
+import Typography from "@material-ui/core/Typography";
+
+const drawerWidth = 240;
 
 const useStyles = makeStyles({
   page: {
     background: "#f9f9f9",
     width: "100%",
+  },
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  root: {
+    display: "flex",
   },
 });
 
@@ -12,7 +25,18 @@ export default function Layout({ children }) {
   const classes = useStyles();
 
   return (
-    <div>
+    <div className={classes.root}>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        anchor="left"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Typography variant="h5">Ninja notes</Typography>
+      </Drawer>
+
       <div className={classes.page}>{children}</div>
     </div>
   );
