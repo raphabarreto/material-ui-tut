d0756c089127f0d9dd316ee3e0a109ae84b67458
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-05 17:08:03 -0300

    More on makeStyles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,23 +11,29 @@ import { useHistory, useLocation } from "react-router-dom";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles({
-  page: {
-    background: "#f9f9f9",
-    width: "100%",
-  },
-  drawer: {
-    width: drawerWidth,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  root: {
-    display: "flex",
-  },
-  active: {
-    background: "#f4f4f4",
-  },
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      background: "#f9f9f9",
+      width: "100%",
+      padding: theme.spacing(3),
+    },
+    drawer: {
+      width: drawerWidth,
+    },
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    root: {
+      display: "flex",
+    },
+    active: {
+      background: "#f4f4f4",
+    },
+    title: {
+      padding: theme.spacing(2),
+    },
+  };
 });
 
 export default function Layout({ children }) {
@@ -59,7 +65,9 @@ export default function Layout({ children }) {
         }}
       >
         <div>
-          <Typography variant="h5">Ninja notes</Typography>
+          <Typography variant="h5" className={classes.title}>
+            Ninja notes
+          </Typography>
         </div>
 
         <List>
